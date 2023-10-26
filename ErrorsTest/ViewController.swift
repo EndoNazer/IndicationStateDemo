8787d66b9bd4137114db2235e7ec548d060e0dcf
Rodin Daniil <Daniil.Rodin@technopark.ru>
2023-10-26 16:22:31 +0400

    Initial Commit

---
+++ ErrorsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErrorsTest
+//
+//  Created by Daniil on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
